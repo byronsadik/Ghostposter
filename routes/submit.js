ff65e8f1ffca50e2b0829f6e20a2b1ca8ed44b0b
Byron <byron@attoconsulting.com>
2016-02-15 11:15:26 -0500

    need a develop branch, rather to use it. master is currently whatever's on heroku.

--- routes/submit.js
+++ routes/submit.js
@@ -17,7 +17,10 @@ router.post('/', function(req, res){
     };
 
 
+    console.log('\n\n\n' + req.body.time + '\n\n\n' + '' + req.body.date + '\n\n\n');
      
+    res.redirect('/');
+
     // the actual post to be submitted
     //
     var submission = {
@@ -33,35 +36,35 @@ router.post('/', function(req, res){
         submission['text'] = req.body.text;
     }
 
-    reddit.auth(credentials, function(err, response) {
-      if (err) {
-        exit("Unable to authenticate user: " + err);
-      } else {
-        // The user is now authenticated. If you want the temporary bearer token, it's available as response.access_token
-        // and will be valid for response.expires_in seconds.
-        // raw.js will automatically refresh the bearer token as it expires. Unlike web apps, no refresh tokens are available.
+    // reddit.auth(credentials, function(err, response) {
+    //   if (err) {
+    //     exit("Unable to authenticate user: " + err);
+    //   } else {
+    //     // The user is now authenticated. If you want the temporary bearer token, it's available as response.access_token
+    //     // and will be valid for response.expires_in seconds.
+    //     // raw.js will automatically refresh the bearer token as it expires. Unlike web apps, no refresh tokens are available.
         
 
-        reddit.captchaNeeded(function(err, required) {
-          if (err) {
-            exit("captchaNeeded failed: " + err);
-          } else {
-            if (required) {
-              exit("can not submit because captcha is needed");
-            } else {
-              reddit.submit(submission, function(err, id) {
-                if (err) {
-                  exit("Unable to submit post: " + err);
-                } else {
-                  console.log("submitted " + id);
-                  res.redirect('/');
-                }
-              });
-            }
-          }
-        });
-      }
-    });
+    //     reddit.captchaNeeded(function(err, required) {
+    //       if (err) {
+    //         exit("captchaNeeded failed: " + err);
+    //       } else {
+    //         if (required) {
+    //           exit("can not submit because captcha is needed");
+    //         } else {
+    //           reddit.submit(submission, function(err, id) {
+    //             if (err) {
+    //               exit("Unable to submit post: " + err);
+    //             } else {
+    //               console.log("submitted " + id);
+    //               res.redirect('/');
+    //             }
+    //           });
+    //         }
+    //       }
+    //     });
+    //   }
+    // }); // end reddit.auth
 
     // in case of error, log to STDERR and exit
     //
