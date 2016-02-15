4422e31b3a85f1a2ac4cb0bbf01800fa8f51d807
Byron <byron@attoconsulting.com>
2016-02-15 11:17:04 -0500

    differentiating master and develop, reverting to current master build, date and time and cronjob not yet configured but date and time objects are being passed in

--- routes/submit.js
+++ routes/submit.js
@@ -16,11 +16,6 @@ router.post('/', function(req, res){
       "password": req.body.userpass,
     };
 
-
-    console.log('\n\n\n' + req.body.time + '\n\n\n' + '' + req.body.date + '\n\n\n');
-     
-    res.redirect('/');
-
     // the actual post to be submitted
     //
     var submission = {
@@ -36,35 +31,35 @@ router.post('/', function(req, res){
         submission['text'] = req.body.text;
     }
 
-    // reddit.auth(credentials, function(err, response) {
-    //   if (err) {
-    //     exit("Unable to authenticate user: " + err);
-    //   } else {
-    //     // The user is now authenticated. If you want the temporary bearer token, it's available as response.access_token
-    //     // and will be valid for response.expires_in seconds.
-    //     // raw.js will automatically refresh the bearer token as it expires. Unlike web apps, no refresh tokens are available.
+    reddit.auth(credentials, function(err, response) {
+      if (err) {
+        exit("Unable to authenticate user: " + err);
+      } else {
+        // The user is now authenticated. If you want the temporary bearer token, it's available as response.access_token
+        // and will be valid for response.expires_in seconds.
+        // raw.js will automatically refresh the bearer token as it expires. Unlike web apps, no refresh tokens are available.
         
 
-    //     reddit.captchaNeeded(function(err, required) {
-    //       if (err) {
-    //         exit("captchaNeeded failed: " + err);
-    //       } else {
-    //         if (required) {
-    //           exit("can not submit because captcha is needed");
-    //         } else {
-    //           reddit.submit(submission, function(err, id) {
-    //             if (err) {
-    //               exit("Unable to submit post: " + err);
-    //             } else {
-    //               console.log("submitted " + id);
-    //               res.redirect('/');
-    //             }
-    //           });
-    //         }
-    //       }
-    //     });
-    //   }
-    // }); // end reddit.auth
+        reddit.captchaNeeded(function(err, required) {
+          if (err) {
+            exit("captchaNeeded failed: " + err);
+          } else {
+            if (required) {
+              exit("can not submit because captcha is needed");
+            } else {
+              reddit.submit(submission, function(err, id) {
+                if (err) {
+                  exit("Unable to submit post: " + err);
+                } else {
+                  console.log("submitted " + id);
+                  res.redirect('/');
+                }
+              });
+            }
+          }
+        });
+      }
+    }); // end reddit.auth
 
     // in case of error, log to STDERR and exit
     //
