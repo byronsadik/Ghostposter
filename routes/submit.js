02ce6414117efa825ecbd9ad47e073dca45f473a
Byron <byron@attoconsulting.com>
2016-02-17 23:40:02 -0500

    redirecting to root after posting, just need to test with actual reddit post

--- routes/submit.js
+++ routes/submit.js
@@ -10,12 +10,12 @@ router.post('/', function(req, res){
 
     reddit.setupOAuth2(req.body.id, req.body.secret);
 
+
+    // prepping the date and time to put into the cronjob
     var time = req.body.time;
     var date = req.body.date;
-
     var hour = ' ' + time.substring(0, 2);
     var minutes = ' ' + time.substring(5, 3);
-
     var day = ' ' + date.substring(date.length, 8);
     var month = ' ' + (date.substring(7, 5) - 1);
 
@@ -41,62 +41,45 @@ router.post('/', function(req, res){
         submission['text'] = req.body.text;
     }
 
-
-    var job = new CronJob(timestamp, function() {
-
-        // actual cron task
-        console.log('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%');
-
-        this.stop();
-
-      }, function () {
-        // gets called after cron job is stopped
-        res.redirect('/');
+    reddit.auth(credentials, function(err, response) {
+      if (err) {
+        exit("Unable to authenticate user: " + err);
+      } else {
+        // The user is now authenticated. If you want the temporary bearer token, it's available as response.access_token
+        // and will be valid for response.expires_in seconds.
+        // raw.js will automatically refresh the bearer token as it expires. Unlike web apps, no refresh tokens are available.
         
+
+        reddit.captchaNeeded(function(err, required) {
+          if (err) {
+            exit("captchaNeeded failed: " + err);
+          } else {
+            if (required) {
+              exit("can not submit because captcha is needed");
+            } else {
+              var job = new CronJob(timestamp, function() {
+                reddit.submit(submission, function(err, id) {
+                  if (err) {
+                    exit("Unable to submit post: " + err);
+                  } else {
+                    console.log("submitted " + id);
+                  }
+                });
+
+                this.stop();
+              });
+
+              job.start();
+
+              res.redirect('/');
+            }
+          }
+        });
       }
-    );
-
-    job.start();
-
-
-    // reddit.auth(credentials, function(err, response) {
-    //   if (err) {
-    //     exit("Unable to authenticate user: " + err);
-    //   } else {
-    //     // The user is now authenticated. If you want the temporary bearer token, it's available as response.access_token
-    //     // and will be valid for response.expires_in seconds.
-    //     // raw.js will automatically refresh the bearer token as it expires. Unlike web apps, no refresh tokens are available.
-        
-
-    //     reddit.captchaNeeded(function(err, required) {
-    //       if (err) {
-    //         exit("captchaNeeded failed: " + err);
-    //       } else {
-    //         if (required) {
-    //           exit("can not submit because captcha is needed");
-    //         } else {
-
-    //           // validate and THEN run cronjob
-
-    //           reddit.submit(submission, function(err, id) {
-    //             if (err) {
-    //               exit("Unable to submit post: " + err);
-    //             } else {
-    //               console.log("submitted " + id);
-    //               res.redirect('/');
-    //             }
-    //           });
-    //         }
-    //       }
-    //     });
-    //   }
-    // }); // end reddit.auth
-
- 
-
+    }); // end reddit.auth
 
     // in case of error, log to STDERR and exit
-    //
+    
     function exit(err) {
       console.error(err);
         res.redirect('/');
